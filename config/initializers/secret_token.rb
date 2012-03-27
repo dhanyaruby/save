66161934f55448cdea4014627c3ed0b3ed8e9e3c
ssriniva <ssriniva@srihari.corporate.thoughtworks.com>
2012-03-27 20:26:23 +0530

    First commit. Seeding the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Save::Application.config.secret_token = 'a3e047169aa65082a2ccfc3c60ba069451a75bb2dff91635dfaf8cb66cf2b48a9b95ededb900d84cbe5d614bf96de0affb8e1ba08b3496cd6b18551357dd0bff'
